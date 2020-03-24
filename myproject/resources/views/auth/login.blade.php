d526d83b92f1d414e4eef02a75a3d9e25490ce1f
pnqanh <quocanh.inbox@gmail.com>
2020-03-24 18:25:32 +0700

    feat: create login form

--- myproject/resources/views/auth/login.blade.php
+++ myproject/resources/views/auth/login.blade.php
@@ -1,1 +1,15 @@
-<h1>Login</h1>
+<div style="text-align: center" class="container">
+    <h1>Login</h1>
+
+    {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+    <div class="form-group">
+        {{ Form::label('title', 'Email') }}
+        {{ Form::text('title', '', ['class' => 'form-control']) }}
+    </div>
+    <div class="form-group">
+        {{ Form::label('body', 'Password') }}
+        {{ Form::text('body', '', ['class' => 'form-control']) }}
+    </div>
+    {{ Form::submit('Login', ['class' => 'btn btn-primary']) }}
+    {!! Form::close() !!}
+</div>
